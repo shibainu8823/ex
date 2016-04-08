a7504577002cd6498bcd15e297036c2c26eac2a0
kentaro-M-D <shibaken8823@gmail.com>
2016-04-08 21:50:48 +0900

    added for

--- sort.c
+++ sort.c
@@ -1,18 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-int list[8];
+#define list_max 8
+int list[list_max-1];
 
 int main(void){
   srand((unsigned)time(NULL));
   int c;
-  for(c=0;c<=7;c++){
+  for(c=0;c<=list_max-1;c++){
     list[c]=rand();
   }
-  for(c=0;c<=7;c++){
+  for(){
+  for(c=0;c<=list_max-1;c++){
+    if(list[c]>list[c+1]){
+      swap(list,c,c+1);}
+  }}
+
+  for(c=0;c<=list_max-1;c++){
     printf("%d\n",list[c]);
   }
 }
 
+int swap(int list[],int a,int b){
+  int temp;
+  temp=list[a];
+  list[a]=list[b];
+  list[b]=temp;
+}
 
   //【課題1】(選択) Small Cでソートプログラムを書け．ソート対象のデータは，サイズが8の一次元int配列の大域変数に格納することとし，main()関数の冒頭で適当な値で埋めることとする．ソートした結果をprint関数を使って昇順で画面に表示せよ．ソーティング・アルゴリズムは好きなものを使って良い．
+
+
+
+
+
