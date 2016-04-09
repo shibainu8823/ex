f448728d2a44e390e0f6743016c9bd9ef6d3b436
kentaro-M-D <shibaken8823@gmail.com>
2016-04-09 10:46:11 +0900

    バブルsotr完成

--- sort.c
+++ sort.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
-#define list_max 8
+#define list_max 80
 int list[list_max-1];
+void print_list(int list[]);
+void swap(int list[],int a,int b);
+void b_sort(int list[]);
+int main(void);
+
 
 int main(void){
   srand((unsigned)time(NULL));
@@ -9,23 +14,39 @@ int main(void){
   for(c=0;c<=list_max-1;c++){
     list[c]=rand();
   }
-  for(b=0;;){
-  for(c=0;c<=list_max-1;c++){
-    if(list[c]>list[c+1]){
-      swap(list,c,c+1);}
-  }}
+  b_sort(list);
+  print_list(list);
+}
 
+void b_sort(int list[]){
+  int c;
+  int b;
   for(c=0;c<=list_max-1;c++){
-    printf("%d\n",list[c]);
+    list[c]=rand();
+  }
+  for(b=0;b<=list_max-1;b++){
+    for(c=0;c<=list_max-2;c++){
+      if(list[c]>list[c+1]){
+    	swap(list,c,c+1);}
+    }
   }
 }
 
-int swap(int list[],int a,int b){
+
+void swap(int list[],int a,int b){
   int temp;
   temp=list[a];
   list[a]=list[b];
   list[b]=temp;
 }
+
+void print_list(int list[]){
+  int c;
+  for(c=0;c<=list_max-1;c++){
+    printf("%d\n",list[c]);
+  }
+}
+
 
   //【課題1】(選択) Small Cでソートプログラムを書け．ソート対象のデータは，サイズが8の一次元int配列の大域変数に格納することとし，main()関数の冒頭で適当な値で埋めることとする．ソートした結果をprint関数を使って昇順で画面に表示せよ．ソーティング・アルゴリズムは好きなものを使って良い．
 //https://github.com/takanabe/introduction-to-git/blob/master/08_rebase.md
